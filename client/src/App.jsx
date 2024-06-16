db06590f7c0f06384285184f35d5502e4617484e
TylerDrd <jainilpatel0909@gmail.com>
2024-06-16 13:37:19 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <main className="p-2.5 max-w-[700px] mx-auto">
+      <header className="flex justify-between mb-12">
+        <a href="" className="no-underline text-[#222] font-bold">My Blog</a>
+        <nav className="flex gap-4">
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
+  )
+}
